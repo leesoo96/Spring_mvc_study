35448ac0510c4d3efcb6dd1287734345a10ecbd4
leesoojin961202 <dltnwls7042@gmail.com>
2020-12-18 15:54:15 +0900

    2020.12.18

--- springMVC/src/main/java/com/LSJ/mvc/StudentController.java
+++ springMVC/src/main/java/com/LSJ/mvc/StudentController.java
@@ -2,6 +2,9 @@ package com.LSJ.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,7 +15,7 @@ public class StudentController {
 	public String studentForm() {
 		return "/student1217/createPage";
 	}
-	
+/*	
 	@RequestMapping("/student/create")
 	public String studentCreate(@ModelAttribute("student") Student student, 
 							     BindingResult result) {
@@ -27,4 +30,25 @@ public class StudentController {
 		
 		return page;
 	}
+*/
+	@RequestMapping("/student/create")
+	public String studentCreate(@ModelAttribute("student") 
+								@Validated Student student, 
+								BindingResult result) {
+		
+		String page = "/student1217/createDonePage";
+		
+		if(result.hasErrors()) {
+			page = "/student1217/createPage";
+		}
+		
+		return page;
+	}
+	
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) {
+		binder.setValidator(new StudentValidator());
+	}
+//	= StudentValidator validator = new StudentValidator();
+//	validator.validate(student, result);
 }
